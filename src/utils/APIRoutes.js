fde70f64f5c1d6fc74286fbdf691b20e715ec0ed
Abzeejay <abiodun.ogunjemilua@gmail.com>
2022-08-11 08:01:09 +0100

    first commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://chatapp-bkend.herokuapp.com/";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/auth/addmsg`;
+export const recieveMessageRoute = `${host}/api/messeages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
